d4fb0e92720b6987bb42d822560e2b172d9d663b
PoorvaRane <poorvarane99@gmail.com>
2018-10-18 12:50:40 -0400

    Create dataset

---
+++ data/data_clean.py
@@ -0,0 +1,40 @@
+import os
+import shutil
+
+def main():
+	"""
+	Download the CelebA dataset from http://mmlab.ie.cuhk.edu.hk/projects/CelebA.html and 
+	run this file to split the dataset into two domains, A and B. Further spilt into 
+	trainA, trainB, testA, testB
+	"""
+
+	dataset_folder = '/data1/prane/CelebA/Img/img_align_celeba/'
+	attr_file_path = '/data1/prane/CelebA/Anno/list_attr_celeba.txt'
+
+	# Define domain A = Male, B = Female
+
+	os.makedirs('trainA', exist_ok=True)
+	os.makedirs('trainB', exist_ok=True)
+	os.makedirs('testA', exist_ok=True)
+	os.makedirs('testB', exist_ok=True)
+
+	# Open the attr file and identify the 'male' attribute
+	attr_file = open(attr_file_path, 'r')
+	image_attrs = attr_file.readlines()
+
+	attr_def = image_attrs[1].split()
+	male_index = attr_def.index('Male') + 1
+
+	# Iterate over each labeled line
+	for attr_line in image_attrs[2:]:
+		image_id = attr_line[0]
+		male_gender = attr_line[male_index]
+
+		if male_gender == '1':
+
+
+
+
+
+if __name__ == '__main__':
+	main()
